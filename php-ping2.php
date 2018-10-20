c512f7610c21da19f928518dcb063044efe2ad7b
miftahulamal <44316908+miftahulamal@users.noreply.github.com>
2018-10-20 11:11:06 +0000

    pingipadd
    pingipadd

---
+++ php-ping2.php
@@ -0,0 +1,407 @@
+<?php
+/*
+ *
+ * Use the examples below to add your own servers. Coded by clone1018 [?]
+ *
+ */
+$title = "PING Server Status"; // website's title
+$servers = array(
+
+    '123.108.11.100' => array(
+        'ip' => '123.108.11.100',
+        'port' => 80,
+        'info' => 'JJSMS',
+        'purpose' => 'Web Search'
+    ),
+    '123.108.11.195' => array(
+        'ip' => '123.108.11.195',
+        'port' => 80,
+        'info' => 'VM',
+        'purpose' => 'No purpose'
+    ),
+    
+    '123.108.11.196' => array(
+        'ip' => '123.108.11.196',
+        'port' => 80,
+        'info' => 'VM',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.11.197' => array(
+        'ip' => '123.108.11.197',
+        'port' => 80,
+        'info' => 'V-SPHERE',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.11.50' => array(
+        'ip' => '123.108.11.50',
+        'port' => 80,
+        'info' => 'ALLBEST',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.104' => array(
+        'ip' => '123.108.10.104',
+        'port' => 80,
+        'info' => 'SIAGA-PULSA',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.117' => array(
+        'ip' => '123.108.10.117',
+        'port' => 80,
+        'info' => 'HADI-CELL',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.18' => array(
+        'ip' => '123.108.10.318',
+        'port' => 80,
+        'info' => 'UMB',
+        'purpose' => 'Web Search'
+    ),
+
+     '123.108.10.23' => array(
+        'ip' => '123.108.10.23',
+        'port' => 80,
+        'info' => '123.108.10.23',
+        'purpose' => 'Web Search'
+    ),
+    '123.108.10.24' => array(
+        'ip' => '123.108.10.24',
+        'port' => 80,
+        'info' => '123.108.10.24',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.25' => array(
+        'ip' => '123.108.10.25',
+        'port' => 80,
+        'info' => '123.108.10.25',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.26' => array(
+        'ip' => '123.108.10.26',
+        'port' => 80,
+        'info' => '123.108.10.26',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.27' => array(
+        'ip' => '123.108.10.27',
+        'port' => 80,
+        'info' => '123.108.10.27',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.28' => array(
+        'ip' => '123.108.10.28',
+        'port' => 80,
+        'info' => '123.108.10.28',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.29' => array(
+        'ip' => '123.108.10.29',
+        'port' => 80,
+        'info' => '123.108.10.29',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.31' => array(
+        'ip' => '123.108.10.31',
+        'port' => 80,
+        'info' => '123.108.10.31',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.33' => array(
+        'ip' => '123.108.10.33',
+        'port' => 80,
+        'info' => '123.108.10.33',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.35' => array(
+        'ip' => '123.108.10.35',
+        'port' => 80,
+        'info' => '123.108.10.35',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.36' => array(
+        'ip' => '123.108.10.36',
+        'port' => 80,
+        'info' => '123.108.10.36',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.37' => array(
+        'ip' => '123.108.10.37',
+        'port' => 80,
+        'info' => '123.108.10.37',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.38' => array(
+        'ip' => '123.108.10.38',
+        'port' => 80,
+        'info' => '123.108.10.38',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.50' => array(
+        'ip' => '123.108.10.50',
+        'port' => 80,
+        'info' => '123.108.10.50',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.52' => array(
+        'ip' => '123.108.10.52',
+        'port' => 80,
+        'info' => '123.108.10.52',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.54' => array(
+        'ip' => '123.108.10.54',
+        'port' => 80,
+        'info' => '123.108.10.54',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.53' => array(
+        'ip' => '123.108.10.53',
+        'port' => 80,
+        'info' => 'GPS-DEV',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.19' => array(
+        'ip' => '123.108.10.19',
+        'port' => 80,
+        'info' => 'USSD-INDOSAT',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.200' => array(
+        'ip' => '123.108.10.200',
+        'port' => 80,
+        'info' => 'GPS',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.201' => array(
+        'ip' => '123.108.10.201',
+        'port' => 80,
+        'info' => 'GPS',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.202' => array(
+        'ip' => '123.108.10.202',
+        'port' => 80,
+        'info' => 'GPS',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.204' => array(
+        'ip' => '123.108.10.204',
+        'port' => 80,
+        'info' => 'KOPAJA.JJTRACK.COM',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.205' => array(
+        'ip' => '123.108.10.205',
+        'port' => 80,
+        'info' => 'PROXMOX',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.206' => array(
+        'ip' => '123.108.10.206',
+        'port' => 80,
+        'info' => '123.108.10.206',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.207' => array(
+        'ip' => '123.108.10.207',
+        'port' => 80,
+        'info' => '123.108.10.207',
+        'purpose' => 'Web Search'
+    ),
+
+    '123.108.10.208' => array(
+        'ip' => '123.108.10.208',
+        'port' => 80,
+        'info' => '123.108.10.208',
+        'purpose' => 'Web Search'
+    ),
+
+);
+
+if (isset($_GET['host'])) {
+    $host = $_GET['host'];
+
+
+    if (isset($servers[$host])) {
+        header('Content-Type: application/json');
+
+        $return = array(
+            'status' => test($servers[$host])
+        );
+
+        echo json_encode($return);
+        exit;
+    } else {
+        header("HTTP/1.1 404 Not Found");
+    }
+}
+
+$names = array();
+foreach ($servers as $name => $info) {
+    $names[$name] = md5($name);
+}
+
+
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $title; ?></title>
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/2.3.2/cosmo/bootstrap.min.css">
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css">
+        <style type="text/css">
+                /* Custom Styles */
+        </style>
+    </head>
+    <body>
+
+        <div class="container">
+            <h3><?php echo $title; ?></h3>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Host</th>
+                        <th>Purpose</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    <?php foreach ($servers as $name => $server): ?>
+
+                        <tr id="<?php echo md5($name); ?>">
+                            <td><i class="icon-spinner icon-spin icon-large"></i></td>
+                            <td class="name"><?php echo $name; ?></td>
+                            <td><?php echo $server['info']; ?></td>
+                            <td><?php echo $server['purpose']; ?></td>
+                        </tr>
+
+                    <?php endforeach; ?>
+
+                </tbody>
+            </table>
+        </div>
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+        <script type="text/javascript">
+
+            function test(host, hash) {
+                // Fork it
+                var request;
+
+                // fire off the request to /form.php
+                request = $.ajax({
+                    url: "<?php echo basename(__FILE__); ?>",
+                    type: "get",
+                    data: {
+                        host: host
+                    },
+                    beforeSend: function () {
+                        $('#' + hash).children().children().css({'visibility': 'visible'});
+                    }
+                });
+
+                // callback handler that will be called on success
+                request.done(function (response, textStatus, jqXHR) {
+                    var status = response.status;
+                    var statusClass;
+                    if (status) {
+                        statusClass = 'success';
+                    } else {
+                        statusClass = 'error';
+                    }
+
+                    $('#' + hash).removeClass('success error').addClass(statusClass);
+                });
+
+                // callback handler that will be called on failure
+                request.fail(function (jqXHR, textStatus, errorThrown) {
+                    // log the error to the console
+                    console.error(
+                        "The following error occured: " +
+                            textStatus, errorThrown
+                    );
+                });
+
+
+                request.always(function () {
+                    $('#' + hash).children().children().css({'visibility': 'hidden'});
+                })
+
+            }
+
+            $(document).ready(function () {
+
+                var servers = <?php echo json_encode($names); ?>;
+                var server, hash;
+
+                for (var key in servers) {
+                    server = key;
+                    hash = servers[key];
+
+                    test(server, hash);
+                    (function loop(server, hash) {
+                        setTimeout(function () {
+                            test(server, hash);
+
+                            loop(server, hash);
+                        }, 6000);
+                    })(server, hash);
+                }
+
+            });
+        </script>
+
+    </body>
+</html>
+<?php
+/* Misc at the bottom */
+function test($server) {
+    $socket = @fsockopen($server['ip'], $server['port'], $errorNo, $errorStr, 3);
+    if ($errorNo == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function in_array_r($needle, $haystack, $strict = false) {
+    foreach ($haystack as $item) {
+        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+?>
